bb96e132b887ca29ca821902c162d09a7f89ad3a
ChaseHutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2024-04-05 23:50:05 -0400

    Added more API calls

--- frontend/api/user.tsx
+++ frontend/api/user.tsx
@@ -14,3 +14,16 @@ export const signUp = (username: string, email: string, password: string) => {
         "password": password
     })
 }
+
+export const forgotPassword = (email: string) => {
+    axios.post(`${API_URL}/forgot-password`, {
+        "email": email
+    })
+}
+export const deleteAccount = () => {
+    axios.post(`${API_URL}/delete-account`,)
+}
+
+export const userData = () => {
+    axios.get(`${API_URL}/me`)
+}
