743e6273ad1a8c0fd63ac83f8d38f889b24356c8
JonFactor <Jon.Factor2@gmail.com>
2024-04-06 11:25:33 -0400

    fixed paths and delete unessisary files

--- ai-testing/main.py
+++ ai-testing/main.py
@@ -1,5 +1,7 @@
 # importing libraries
-import numpy, cv2, time
+import numpy, cv2, time, os
+from PIL import Image
+import torch
 
 # capturing video in real time
 cap = cv2.VideoCapture(1)
@@ -8,8 +10,22 @@ cap = cv2.VideoCapture(1)
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 
-def objRecognition(frame):
-    print(str(frame))
+def objRecognition():
+    # os.system("python ./ai-testing/yolov5/detect.py --weights ai-testing\\yolov5\\runs\\train\exp6\\weights\\best.pt --source ./ai-testing/curImg.png")
+    # print("1")
+
+
+
+    # Model
+    model = torch.hub.load(r'./ai-testing/yolov5/runs/train/exp6/weights/best.pt', 'best', source='local')
+
+    # Image
+    im = cv2.imread(".\\ai-testing\\curImg.png")
+
+    # Inference
+    results = model(im)
+    print(results)
+
 
 while cap.isOpened():
 
@@ -27,12 +43,13 @@ while cap.isOpened():
         if cv2.contourArea(contour) < 30000:
             continue
         time.sleep(5)
-        objRecognition(frame2)
+        cv2.imwrite("ai-testing/curImg.png", frame2)
+        objRecognition()
     frame1 = frame2
     ret, frame2 = cap.read()
 
-    if cv2.waitKey(50) == 27:
-        break
+    # if cv2.waitKey(50) == 27:
+    #     break
 
 cap.release()
 cv2.destroyAllWindows()
--- food-recognition-4/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# food recognition > 2024-04-06 2:08am
-https://universe.roboflow.com/hungarai/food-recognition-q6fph
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
