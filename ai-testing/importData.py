1948840457105137f16f418cac09df495dbe994f
JonFactor <Jon.Factor2@gmail.com>
2024-04-06 03:10:49 -0400

    add data

--- ai-testing/importData.py
+++ ai-testing/importData.py
@@ -4,5 +4,5 @@ from roboflow import Roboflow
 rf = Roboflow(api_key="fz1COejid226SE3nf49I")
 project = rf.workspace("hungarai").project("food-recognition-q6fph")
 version = project.version(4)
-dataset = version.download("yolov5")
+dataset = version.download("yolov5", "./ai-testing/data")
 
