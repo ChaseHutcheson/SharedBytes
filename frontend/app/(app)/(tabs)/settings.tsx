f125b3232aa6684e590203c8b525200cebe8f635
Noah Diana <noah.diana10312005@gmail.com>
2024-04-06 21:49:41 -0400

    added native base

--- frontend/app/(app)/(tabs)/settings.tsx
+++ frontend/app/(app)/(tabs)/settings.tsx
@@ -1,17 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-import { useAuth } from '@/context/AuthContext';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { useAuth } from "@/context/AuthContext";
+import { Box } from "native-base";
 
 export default function TabTwoScreen() {
-  const { user } = useAuth()
+  const { user } = useAuth();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{user?.id}</Text>
-      <Text style={styles.title}>{user?.username}</Text>
-      <Text style={styles.title}>{user?.email}</Text>
+      <Box>
+        <Text style={styles.title}>{user?.id}</Text>
+        <Text style={styles.title}>{user?.username}</Text>
+        <Text style={styles.title}>{user?.email}</Text>
+      </Box>
     </View>
   );
 }
@@ -19,16 +22,16 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
--- frontend/app/(app)/_layout.tsx
+++ frontend/app/(app)/_layout.tsx
@@ -1,20 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useAuth } from '@/context/AuthContext';
+import React from "react";
+import { Stack } from "expo-router";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useAuth } from "@/context/AuthContext";
+import { NativeBaseProvider } from "native-base";
 
 export default function _layout() {
-    const colorScheme = useColorScheme();
-    const { isAuthenticated } = useAuth()
+  const colorScheme = useColorScheme();
+  const { isAuthenticated } = useAuth();
 
-    return (
-        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-
-                    <Stack>
-                        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                    </Stack>
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <NativeBaseProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </NativeBaseProvider>
+    </ThemeProvider>
+  );
 }
