4577ac248d7a880fbc41dddaf82486c8cd57eb5f
Noah Diana <noah.diana10312005@gmail.com>
2024-04-07 00:12:39 -0400

    added profile section screen layout

--- frontend/app/(app)/(tabs)/settings.tsx
+++ frontend/app/(app)/(tabs)/settings.tsx
@@ -1,37 +1,87 @@
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+import {
+  Box,
+  Center,
+  FormControl,
+  Icon,
+  Input,
+  Text,
+  VStack,
+  Button,
+  ScrollView,
+} from "native-base";
 import { useAuth } from "@/context/AuthContext";
-import { Box } from "native-base";
+import { Feather, AntDesign } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { TouchableOpacity } from "react-native";
+import { Form } from "react-hook-form";
 
 export default function TabTwoScreen() {
   const { user } = useAuth();
+  const router = useRouter();
+
+  if (!user) {
+    return null;
+  }
 
   return (
-    <View style={styles.container}>
-      <Box>
-        <Text style={styles.title}>{user?.id}</Text>
-        <Text style={styles.title}>{user?.username}</Text>
-        <Text style={styles.title}>{user?.email}</Text>
+    <ScrollView>
+      <Box flex={1} alignItems="center" justifyContent="flex-start" pt={12}>
+        <VStack alignItems="center" space={4}>
+          <Box
+            bgColor="gray.200"
+            borderRadius={90}
+            width={40}
+            height={40}
+            m={6}
+          >
+            <Center>
+              <Icon as={Feather} name="user" size={20} my={8} />
+            </Center>
+          </Box>
+
+          <Text fontSize="3xl" fontWeight="bold">
+            {user?.username}
+          </Text>
+          <Text fontSize="lg" color="gray.500">
+            {user?.email}
+          </Text>
+        </VStack>
+
+        <Box w="80%" p={4} my={5} rounded="md" bg="white" shadow={1}>
+          <FormControl isReadOnly>
+            <FormControl.Label>Current Email</FormControl.Label>
+            <Input
+              placeholder={user?.email}
+              isReadOnly={true}
+              color="gray.500"
+            />
+            <FormControl.Label>New Email</FormControl.Label>
+            <Input placeholder="New Email" type="text" />
+            <Button mt={5} colorScheme="blue" onPress={() => {}}>
+              Save Changes
+            </Button>
+          </FormControl>
+        </Box>
+
+        <Box w="80%" p={4} my={5} rounded="md" bg="white" shadow={1}>
+          <FormControl>
+            <FormControl.Label>Current Password</FormControl.Label>
+            <Input
+              placeholder="Current Password"
+              type="password"
+              isReadOnly={true}
+              color="gray.500"
+            />
+            <FormControl.Label>New Password</FormControl.Label>
+            <Input placeholder="New Password" type="password" />
+            <FormControl.Label>Confirm New Password</FormControl.Label>
+            <Input placeholder="Confirm New Password" type="password" />
+            <Button mt={5} colorScheme="blue" onPress={() => {}}>
+              Save Changes
+            </Button>
+          </FormControl>
+        </Box>
       </Box>
-    </View>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -5,81 +5,89 @@ import * as SecureStore from "expo-secure-store";
 import { getMe } from "@/api/users";
 import { signIn, signUp } from "@/api/auth";
 
-
 interface IAuthContext {
-    user: User | null;
-    authToken: string | null;
-    isAuthenticated: boolean;
-    isLoading: boolean;
-    contextSignIn: (email: string, password: string) => Promise<void>;
-    contextSignUp: (
-        username: string,
-        email: string,
-        password: string,
-    ) => Promise<void>;
+  user: User | null;
+  authToken: string | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  contextSignIn: (email: string, password: string) => Promise<void>;
+  contextSignUp: (
+    username: string,
+    email: string,
+    password: string
+  ) => Promise<void>;
 }
 
 const AuthContext = createContext<IAuthContext>({
-    user: null,
-    authToken: null,
-    isAuthenticated: false,
-    isLoading: false,
-    contextSignIn: async () => { },
-    contextSignUp: async () => { },
+  user: null,
+  authToken: null,
+  isAuthenticated: false,
+  isLoading: false,
+  contextSignIn: async () => {},
+  contextSignUp: async () => {},
 });
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }: any) => {
-    const [user, setUser] = useState<User | null>(null);
-    const [authToken, setAuthToken] = useState<string | null>(null);
-    const [isAuthenticated, setAuthenticated] = useState<boolean>(false);
-    const [isLoading, setLoading] = useState<boolean>(true);
+  const [user, setUser] = useState<User | null>(null);
+  const [authToken, setAuthToken] = useState<string | null>(null);
+  const [isAuthenticated, setAuthenticated] = useState<boolean>(false);
+  const [isLoading, setLoading] = useState<boolean>(true);
 
-    const contextSignIn = async (email: string, password: string) => {
-        setLoading(true)
-        try {
-            const tokens = await signIn(email, password);
-            console.log(tokens.access_token)
-            const user: User = await getMe(tokens.access_token);
+  const contextSignIn = async (email: string, password: string) => {
+    setLoading(true);
+    try {
+      const tokens = await signIn(email, password);
+      console.log(tokens.access_token);
+      const user: User = await getMe(tokens.access_token);
 
-            await SecureStore.setItemAsync("access_token", tokens.access_token);
+      await SecureStore.setItemAsync("access_token", tokens.access_token);
 
-            setUser(user);
-            setAuthToken(tokens.access_token);
-            setAuthenticated(true);
-            setLoading(false);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+      setUser(user);
+      setAuthToken(tokens.access_token);
+      setAuthenticated(true);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const contextSignUp = async (
-        username: string,
-        email: string,
-        password: string,
-    ) => {
-        setLoading(true);
-        try {
-            const tokens = await signUp(username, email, password)
-            console.log(tokens)
-            console.log(tokens.access_token)
-            const user: User = await getMe(tokens.access_token)
+  const contextSignUp = async (
+    username: string,
+    email: string,
+    password: string
+  ) => {
+    setLoading(true);
+    try {
+      const tokens = await signUp(username, email, password);
+      console.log(tokens);
+      console.log(tokens.access_token);
+      const user: User = await getMe(tokens.access_token);
 
-            await SecureStore.setItemAsync("access_token", tokens.access_token);
+      await SecureStore.setItemAsync("access_token", tokens.access_token);
 
-            setUser(user);
-            setAuthToken(tokens.access_token);
-            setAuthenticated(true);
-            setLoading(false);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+      setUser(user);
+      setAuthToken(tokens.access_token);
+      setAuthenticated(true);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <AuthContext.Provider value={{ user, authToken, isAuthenticated, isLoading, contextSignIn, contextSignUp }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        authToken,
+        isAuthenticated,
+        isLoading,
+        contextSignIn,
+        contextSignUp,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
